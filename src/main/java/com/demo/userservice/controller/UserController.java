e21b51b476018ef87ffefd1df88f4a1bc64bd426
Abhishek Ralhan <abhishekr@bitwiseglobal.net>
2019-09-17 14:21:48 +0530

    initial changes

---
+++ src/main/java/com/demo/userservice/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.demo.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class UserController {
+
+	@GetMapping("/user/product")
+	public Product getProduct() {
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate.getForObject("http://localhost:9090/product", Product.class);
+	}
+
+}
